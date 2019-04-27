727aa162f42f503e2e2dcf0c09f13751b85b6ca4
rookieery <49903818+rookieery@users.noreply.github.com>
2019-04-27 20:54:41 +0800

    猜拳游戏

---
+++ 小游戏/Person.java
@@ -0,0 +1,39 @@
+package 小游戏;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: GAOBO
+ * Date: 2019-04-06
+ * Time: 14:45
+ */
+public class Person {
+    private String name;
+    private int score;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore() {
+        this.score++;
+    }
+
+    //出拳
+    public String fist() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入出的招数：石头，剪刀，布");
+        String str = scanner.nextLine();
+        return str;
+    }
+}
